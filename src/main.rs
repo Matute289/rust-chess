91059987ddd0bcb08453af4713a5af4adacca230
mgrinberg <matias.grinberg@mercadolibre.com>
2021-03-26 23:53:41 -0300

    create board. creating pieces

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,130 @@
+mod pieces;
+
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    App::build()
+        .add_resource(Msaa{samples:4})
+        .add_resource(WindowDescriptor {
+            title: "Ajedrez".to_string(),
+            width: 1200.,
+            height: 1000.,
+            ..Default::default()
+        })
+        .add_plugins(DefaultPlugins)
+        .add_startup_system(setup.system())
+        .add_startup_system(create_board.system())
+        .add_startup_system(create_pieces.system())
+        .run();
+}
+
+fn setup(
+    commands: &mut Commands,
+){
+    commands
+    // Plano
+        /*.spawn(PbrBundle{
+            mesh: meshes.add(Mesh::from(shape::Plane{size: 8.0})),
+            material: materials.add(Color::rgb(1.,0.9,0.9).into()),
+            transform: Transform::from_translation(Vec3::new(4.,0., 4.)),
+            ..Default::default()
+        })*/
+    // Camara
+        .spawn(Camera3dBundle {
+            transform: Transform::from_matrix(Mat4::from_rotation_translation(
+                Quat::from_xyzw(-0.3,-0.5,-0.3,0.5).normalize(),
+                Vec3::new(-4.0, 12.0,4.0),
+            )),
+            ..Default::default()
+        })
+    // Luces
+        .spawn(LightBundle{
+            transform: Transform::from_translation(Vec3::new(4.0, 8.0, 4.0)),
+            ..Default::default()
+        });
+}
+
+fn create_board(
+    commands: &mut Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    // Add meshes and materials
+    let mesh = meshes.add(Mesh::from(shape::Plane{size: 1.}));
+    let white_material = materials.add(Color::rgb(1., 0.9, 0.9).into());
+    let black_material = materials.add(Color::rgb(0., 0.1, 0.1).into());
+
+    // Spawn 64 squares
+    for i in 0..8 {
+        for j in 0..8 {
+            commands.spawn( PbrBundle{
+                mesh: mesh.clone(),
+                material: if (i+j+1) % 2 == 0 {
+                    white_material.clone()
+                } else {
+                    black_material.clone()
+                },
+                transform: Transform::from_translation(Vec3::new(i as f32, 0., j as f32)),
+                ..Default::default()
+            });
+        }
+    }
+}
+
+fn create_pieces(
+    commands: &mut Commands,
+    asset_server: Res<AssetServer>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    // Load all the meshes
+    let king_handle: Handle<Mesh> =
+        asset_server.load("models/chess_kit/pieces.glb#Mesh0/Primitive0");
+    let king_cross_handle: Handle<Mesh> =
+        asset_server.load("models/chess_kit/pieces.glb#Mesh1/Primitive0");
+    let pawn_handle: Handle<Mesh> =
+        asset_server.load("models/chess_kit/pieces.glb#Mesh2/Primitive0");
+    let knight_1_handle: Handle<Mesh> =
+        asset_server.load("models/chess_kit/pieces.glb#Mesh3/Primitive0");
+    let knight_2_handle: Handle<Mesh> =
+        asset_server.load("models/chess_kit/pieces.glb#Mesh4/Primitive0");
+    let rook_handle: Handle<Mesh> =
+        asset_server.load("models/chess_kit/pieces.glb#Mesh5/Primitive0");
+    let bishop_handle: Handle<Mesh> =
+        asset_server.load("models/chess_kit/pieces.glb#Mesh6/Primitive0");
+    let queen_handle: Handle<Mesh> =
+        asset_server.load("models/chess_kit/pieces.glb#Mesh7/Primitive0");
+
+    // Add some materials
+    let white_material = materials.add(Color::rgb(1., 0.8, 0.8).into());
+    let black_material = materials.add(Color::rgb(0., 0.2, 0.2).into());
+
+    /*commands
+    // Spawn parent entity
+        .spawn(PbrBundle{
+            transform: Transform::from_translation(Vec3::new(0.0, 0.0, 4.0)),
+            ..Default::default()
+        })
+    // Add children to the parent
+        .with_children(|parent|{
+            parent.spawn(PbrBundle{
+                mesh: king_handle.clone(),
+                material: white_material.clone(),
+                transform: {
+                    let mut transform = Transform::from_translation(Vec3::new(-0.2,0.,-1.9));
+                    transform.apply_non_uniform_scale(Vec3::new(0.2, 0.2, 0.2));
+                    transform
+                },
+                ..Default::default()
+            });
+            parent.spawn(PbrBundle{
+                mesh: king_cross_handle.clone(),
+                material: white_material.clone(),
+                transform: {
+                    let mut transform = Transform::from_translation(Vec3::new(-0.2, 0., -1.9));
+                    transform.apply_non_uniform_scale(Vec3::new(0.2, 0.2, 0.2));
+                    transform
+                },
+                ..Default::default()
+            });
+        });*/
 }
